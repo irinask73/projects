2c73001e88b03f9b95d045819b09d71aa616291c
irinask73 <irinaskid@gmail.com>
2021-02-03 18:10:02 +0200

    feat: add animation header menu in mobile version

--- bigBear/pages/main/main.js
+++ bigBear/pages/main/main.js
@@ -1,15 +1,16 @@
-
+//header menu start
 const mobileMenuBtn = document.querySelector('[data-mobileMenuBtn]');
 const header = document.querySelector('[data-header]');
 const headerMenu = document.querySelector('[data-headerMenu]');
 const menuLeft = document.querySelector('[data-menuLeft]');
 const menuRight = document.querySelector('[data-menuRight]');
 
-mobileMenuBtn.addEventListener('click', function(){
-    mobileMenuBtn.classList.toggle('btn--active');
-    header.classList.toggle('header__active');
-    headerMenu.classList.toggle('header__menu--active');
-    menuLeft.classList.toggle('header__menu-left--active');
-    menuRight.classList.toggle('header__menu-right--active');
+mobileMenuBtn.addEventListener('click', function () {
+    mobileMenuBtn.classList.toggle('active');
+    header.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    menuLeft.classList.toggle('active');
+    menuRight.classList.toggle('active');
 });
+//header menu end
 
