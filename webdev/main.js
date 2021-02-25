f018081acc272a55107f1903f5d198ec24ba9db5
irinask73 <irinaskid@gmail.com>
2021-02-25 23:04:04 +0200

    feat: add new layout and adaptive webdev

---
+++ webdev/main.js
@@ -0,0 +1,34 @@
+const bigBearSwiper = new Swiper('.user__slider.swiper-container', {
+    spaceBetween: 10,
+    loop: true,
+    mousewheel: true,
+    navigation: {
+      nextEl: '.nxt--btn',
+      prevEl: '.prv--btn',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      300: {
+        direction: 'vertical',
+        slidesPerView: 1
+      },
+      700: {
+        slidesPerView: 1.2
+      },
+      900: {
+        slidesPerView: 1.8
+      },
+      1000: {
+        slidesPerView: 2.1
+      },
+      1600: {
+        slidesPerView: 2.5
+      },
+    },
+  });
+  
+  
