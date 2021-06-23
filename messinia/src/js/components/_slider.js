bced86c003769ed35a8170c4c412b841d30ab26a
irinask73 <irinaskid@gmail.com>
2021-06-23 15:42:12 +0300

    feat: add slider in section clients

--- messinia/src/js/components/_slider.js
+++ messinia/src/js/components/_slider.js
@@ -1,27 +1,26 @@
 const slider = new Swiper('.swiper-container', {
-  // Optional parameters
-  effect: 'coverflow',
   grabCursor: true,
   centeredSlides: true,
   loop: true,
   spaceBeteeen: 0,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-},
-autoplay: {
+  autoplay: {
     delay: 5000,
-},
-keyboard: {
+  },
+  keyboard: {
     enabled: true,
-},
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
+});
+
+//clients
+const clientsSwiper = new Swiper('.clients__slider', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
